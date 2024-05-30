c4f74d170b141f79e3182867e5258305d04020c0
Artur <artur-mamedbekov@yandex.ru>
2024-05-30 23:33:49 +0300

    transfer ownership to multisig

---
+++ deploy/04_transferOwner/04_earlyBirdTier3Badge.js
@@ -0,0 +1,10 @@
+const { migration } = require('../../scripts/deploy');
+const hardhat = require('hardhat');
+const BN = require('big.js');
+
+module.exports = migration(async (deployer) => {
+  const multisig = await deployer.getContract('GovernorMultisig');
+
+  return deployer.execute('EarlyBirdTier3Badge', 'transferOwnership', [await multisig.getAddress()]);
+});
+module.exports.tags = ['Upgradable'];
