190297e20d60b44cef4cab85159906b952af29b1
Artur <artur-mamedbekov@yandex.ru>
2024-06-05 12:19:04 +0300

    price feed constructor args

--- deploy/mock/priceFeed/01_deployPriceFeed.js
+++ deploy/mock/priceFeed/01_deployPriceFeed.js
@@ -3,6 +3,7 @@ const { migration } = require('../../../scripts/deploy');
 module.exports = migration(async (deployer) => {
   await deployer.deploy('contracts/mock/PriceFeedMock.sol:PriceFeedMock', {
     name: 'PriceFeedMock',
+    args: ['8'],
   });
 });
 module.exports.tags = ['Mock', 'NonUpgradable'];
