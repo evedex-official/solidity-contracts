e63379e664cc7abb4a65fff64479df571d9a5467
Artur <artur-mamedbekov@yandex.ru>
2024-06-05 12:53:00 +0300

    multisig deploy

--- deploy/05_multicall3/01_deployMulticall.js
+++ deploy/05_multicall3/01_deployMulticall.js
@@ -1,9 +1,8 @@
 const { migration } = require('../../scripts/deploy');
 
 module.exports = migration(async (deployer) => {
-  await deployer.deployProxy('contracts/multicall/Multicall3.sol:Multicall3', {
+  await deployer.deploy('contracts/multicall/Multicall3.sol:Multicall3', {
     name: 'Multicall3',
-    args: [],
   });
 });
 module.exports.tags = ['NonUpgradable'];
