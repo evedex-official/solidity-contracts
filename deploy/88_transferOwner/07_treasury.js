b9783821f0852dc2802a966953c825861c855ea9
Vladislav Komissarov <mastyf@gmail.com>
2024-06-10 16:45:57 +0200

    fix: Treasury

--- deploy/88_transferOwner/07_treasury.js
+++ deploy/88_transferOwner/07_treasury.js
@@ -2,7 +2,7 @@ const { migration } = require('../../scripts/deploy');
 const hardhat = require('hardhat');
 
 module.exports = migration(async (deployer) => {
-  const multisig = await deployer.getContract('Treasury');
+  const multisig = await deployer.getContract('GovernorMultisig');
 
   return deployer.execute('Treasury', 'transferOwnership', [await multisig.getAddress()]);
 });
