b614c1ad28b704683a7a3dfb86874aa3f6c2b253
Vladislav Komissarov <mastyf@gmail.com>
2024-06-10 14:14:14 +0200

    TreasuryUpgradable - transferOwnership

---
+++ deploy/88_transferOwner/07_treasury.js
@@ -0,0 +1,9 @@
+const { migration } = require('../../scripts/deploy');
+const hardhat = require('hardhat');
+
+module.exports = migration(async (deployer) => {
+  const multisig = await deployer.getContract('TreasuryUpgradable');
+
+  return deployer.execute('TreasuryUpgradable', 'transferOwnership', [await multisig.getAddress()]);
+});
+module.exports.tags = ['Governance'];
