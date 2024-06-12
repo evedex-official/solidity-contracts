63e31f0fdc84061d2383d6afb2bdc6d5058bb0d3
Artur <artur-mamedbekov@yandex.ru>
2024-06-12 12:27:12 +0300

    transfer ownership for burn registry

---
+++ deploy/88_transferOwner/08_burnRegistry/01_burnRegistry.js
@@ -0,0 +1,9 @@
+const { migration } = require('../../scripts/deploy');
+const hardhat = require('hardhat');
+
+module.exports = migration(async (deployer) => {
+  const multisig = await deployer.getContract('GovernorMultisig');
+
+  return deployer.execute('BurnRegistry', 'transferOwnership', [await multisig.getAddress()]);
+});
+module.exports.tags = ['Upgradable'];
