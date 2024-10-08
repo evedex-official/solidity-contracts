5518074618cb879c6d7622ba309d477a062155bb
Vladislav Komissarov <mastyf@gmail.com>
2024-10-08 20:19:02 +0200

    fix: remove opstack network

--- hardhat.config.js
+++ hardhat.config.js
@@ -87,13 +87,6 @@ module.exports = {
       blockGasLimit: 30_000_000,
       accounts: accounts('DEPLOYER'),
     },
-    eventum_testnet_opstack: {
-      url: process.env.EVENTUM_TESTNET_OPSTACK_NODE,
-      chainId: 16181,
-      gasPrice: 1_000_000_000,
-      blockGasLimit: 100_000_000,
-      accounts: accounts('DEPLOYER'),
-    },
   },
   namedAccounts: {
     deployer: {
