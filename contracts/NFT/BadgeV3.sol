a97dfd678451c78d63d4e458d8c08997e210d6b8
Artur <artur-mamedbekov@yandex.ru>
2024-10-16 12:26:13 +0300

    badge 3

---
+++ contracts/NFT/BadgeV3.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.20;
+
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {BadgeV1} from "./BadgeV1.sol";
+import {BurnRegistryV1} from "./burnRegistry/BurnRegistryV1.sol";
+
+contract BadgeV3 is BadgeV1 {
+  using MessageHashUtils for bytes32;
+  using ECDSA for bytes32;
+
+  error BadgeV3InvalidBurnSignature();
+
+  struct BurnForPayload {
+    bytes32 id; // Equals of _id field.
+    uint256 tokenId; // Burned token ID.
+    bytes signature; // Signature.
+  }
+
+  constructor() {
+    _disableInitializers();
+  }
+
+  /// @param payloads Payloads package (256 elements max).
+  function burnForBulk(BurnForPayload[] memory payloads) external {
+    for (uint8 i = 0; i < payloads.length; i++) {
+      BurnForPayload memory payload = payloads[i];
+      bytes32 signedMessage = keccak256(abi.encodePacked(payload.id, payload.tokenId));
+      if (payload.id != _id) {
+        revert BadgeV3InvalidBurnSignature();
+      }
+      if (signedMessage.toEthSignedMessageHash().recover(payload.signature) != _signer) {
+        revert BadgeV3InvalidBurnSignature();
+      }
+
+      _burn(payload.tokenId);
+      BurnRegistryV1(_burnRegistry).burn(ownerOf(payload.tokenId), payload.tokenId);
+    }
+  }
+}
