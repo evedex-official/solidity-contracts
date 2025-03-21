ea51577c8b99689b6f689b92be576c21529d0783
Artur <artur-mamedbekov@yandex.ru>
2025-03-21 18:13:54 +0300

    bridge middleware for default bridge

--- contracts/bridgeMiddleware/BridgeMiddleware.sol
+++ contracts/bridgeMiddleware/BridgeMiddleware.sol
@@ -7,6 +7,10 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {Storage} from "../storage/Storage.sol";
 
+interface DefaultBridgeGateway {
+  function getGateway(address _token) external view returns (address gateway);
+}
+
 contract BridgeMiddleware is Context, Initializable {
   using SafeERC20 for IERC20;
 
@@ -35,6 +39,18 @@ contract BridgeMiddleware is Context, Initializable {
     owner = _owner;
   }
 
+  function refund(address token, uint256 amount) external {
+    if (_msgSender() != owner) {
+      revert Forbidden();
+    }
+
+    if (token == address(0)) {
+      payable(owner).transfer(amount);
+    } else {
+      IERC20(token).safeTransfer(owner, amount);
+    }
+  }
+
   function _safeApprove(address token, address spender, uint256 amount) internal {
     uint256 allowance = IERC20(token).allowance(address(this), spender);
     if (allowance >= amount) return;
@@ -43,13 +59,38 @@ contract BridgeMiddleware is Context, Initializable {
     IERC20(token).approve(spender, amount);
   }
 
-  function deposit(bytes32 bridgeName, address token, uint256 amount, bytes memory data) external payable {
+  function depositDefault(address token, uint256 amount, bytes memory data) external payable {
     bool isCallAllowed = Storage(info).getBool(
       keccak256(abi.encodePacked("EH:BridgeMiddleware:Depositor:", _msgSender()))
     );
     if (!isCallAllowed) revert Forbidden();
 
-    address bridge = Storage(info).getAddress(bridgeName);
+    address bridge = Storage(info).getAddress(keccak256("EH:BridgeMiddleware:Bridge:Default"));
+    if (bridge == address(0)) revert BridgeNotFound();
+
+    address gateway = DefaultBridgeGateway(bridge).getGateway(token);
+
+    bool success;
+    if (token == address(0)) {
+      // solhint-disable-next-line avoid-low-level-calls
+      (success, ) = bridge.call{value: amount}(data);
+    } else {
+      _safeApprove(token, gateway, amount);
+      // solhint-disable-next-line avoid-low-level-calls
+      (success, ) = bridge.call{value: msg.value}(data);
+    }
+    if (!success) revert DepositFailed();
+
+    emit Deposit(token, amount);
+  }
+
+  function depositAcross(address token, uint256 amount, bytes memory data) external payable {
+    bool isCallAllowed = Storage(info).getBool(
+      keccak256(abi.encodePacked("EH:BridgeMiddleware:Depositor:", _msgSender()))
+    );
+    if (!isCallAllowed) revert Forbidden();
+
+    address bridge = Storage(info).getAddress(keccak256("EH:BridgeMiddleware:Bridge:Across"));
     if (bridge == address(0)) revert BridgeNotFound();
 
     bool success;
