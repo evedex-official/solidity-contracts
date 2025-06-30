8effc03c4767bc392c192c8d23172341989def70
shcube <shandin.va@gmail.com>
2025-06-30 14:45:47 +0300

    fix(deploy): deploy both versions of bridgeMiddleware

--- deploy/08_bridgeMiddleware/02_bridgeMiddleware.js
+++ deploy/08_bridgeMiddleware/02_bridgeMiddleware.js
@@ -1,6 +1,9 @@
 const { migration } = require('../../scripts/deploy');
 
 module.exports = migration(async (deployer) => {
+  await deployer.deploy('contracts/bridgeMiddleware/BridgeMiddlewareV1.sol:BridgeMiddlewareV1', {
+    name: 'BridgeMiddlewareV1',
+  });
   await deployer.deploy('contracts/bridgeMiddleware/BridgeMiddlewareV2.sol:BridgeMiddlewareV2', {
     name: 'BridgeMiddlewareV2',
   });
