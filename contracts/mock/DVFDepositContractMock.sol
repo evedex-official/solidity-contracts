09f6f1acc8ed6fb2be1c43df9c57ea323bc63fa3
Vladislav Komissarov <mastyf@gmail.com>
2025-07-16 13:34:26 +0200

    fix: BridgeMiddlewareV2

--- contracts/mock/DVFDepositContractMock.sol
+++ contracts/mock/DVFDepositContractMock.sol
@@ -4,18 +4,7 @@ pragma solidity ^0.8.20;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
-interface IERC20Permit {
-  function permit(
-    address owner,
-    address spender,
-    uint256 value,
-    uint256 deadline,
-    uint8 v,
-    bytes32 r,
-    bytes32 s
-  ) external;
-}
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
 contract DVFDepositContractMock is Ownable {
   using SafeERC20 for IERC20;
