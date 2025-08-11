90e9300633e89c35a2a8810e899f53992719b0af
shcube <shandin.va@gmail.com>
2025-08-11 11:57:24 +0300

    Add environment variable checks for payment token and billing managers in deployment script

--- deploy/13_billing/01_deployBilling.js
+++ deploy/13_billing/01_deployBilling.js
@@ -1,8 +1,23 @@
+const { ethers } = require('ethers');
 const { migration } = require('../../scripts/deploy');
 const hardhat = require('hardhat');
 
 module.exports = migration(async (deployer) => {
   const storage = await deployer.getContract('Storage');
+
+  const paymentToken = process.env[`${hardhat.network.name}_BILLING_PAYMENT_TOKEN`];
+  if (!paymentToken) {
+    console.error(`⚠️ Environment variable ${hardhat.network.name}_BILLING_PAYMENT_TOKEN is not set.`);
+    console.log('⚠️  WARNING! Do not forget to set payment token in storage. Key for it: "EH:Billing:PaymentToken"');
+    return;
+  }
+  const billingManagers = process.env[`${hardhat.network.name}_BILLING_MANAGERS`]
+    ? JSON.parse(process.env[`${hardhat.network.name}_BILLING_MANAGERS`])
+    : [];
+  if (billingManagers.length === 0) {
+    console.error(`⚠️ Environment variable ${hardhat.network.name}_BILLING_MANAGERS is not set or empty.`);
+    console.log('⚠️  WARNING! Do not forget to set managers in storage. Key for them: "EH:Billing:Manager:"');
+  }
 
   await deployer.deployProxy('contracts/billing/Billing.sol:Billing', {
     name: 'Billing',
@@ -11,8 +26,26 @@ module.exports = migration(async (deployer) => {
     initializer: 'initialize',
   });
 
-  console.log('⚠️  WARNING! Do not forget to set managers in storage. Key for them: "EH:Billing:Manager:"');
-  console.log('⚠️  WARNING! Do not forget to set payment token in storage. Key for it: "EH:Billing:PaymentToken"');
+  if (paymentToken) {
+    try {
+      await storage.setAddress(ethers.id('EH:Billing:PaymentToken'), paymentToken);
+      console.log(`✅ Set payment token: ${paymentToken}`);
+    } catch (e) {
+      console.error(
+        `❌ Failed to set payment token ${paymentToken}. Probably ownership of Storage is transferred to multisig.`,
+      );
+    }
+  }
+  for (const manager of billingManagers) {
+    try {
+      await storage.setAddress(ethers.id('EH:Billing:Manager:'), manager);
+      console.log(`✅ Set billing manager: ${manager}`);
+    } catch (e) {
+      console.error(
+        `❌ Failed to set billing manager ${manager}. Probably ownership of Storage is transferred to multisig.`,
+      );
+    }
+  }
 });
 
 module.exports.tags = ['DeployBilling', 'Managers', 'Upgradable'];
